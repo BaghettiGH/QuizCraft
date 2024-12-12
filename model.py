5f687f03eb5119b9e0e06c001f9bdbdc0af01dc4
BaghettiGH <trollolol846@gmail.com>
2024-12-12 16:27:28 +0800

    parameter problems

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 
 generator = pipeline(model = "mrm8488/t5-base-finetuned-question-generation-ap")
-res = generator(
+res = generator("hello"
 
-    
+
 )
+print(res)
