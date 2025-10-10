bf02c09c75c8d47e6434d091d641edb8a97c1e56
BaghettiGH <trollolol846@gmail.com>
2025-10-10 19:06:30 +0800

    db ok

--- backend/app/api/study_material.py
+++ backend/app/api/study_material.py
@@ -3,13 +3,13 @@ from app.services.supabase_client import get_supabase
 from app.schemas.study_material import StudyMaterialCreate
 router = APIRouter()
 
-@router.get("/study_materials")
+@router.get("/study_material")
 def get_materials():
     supabase = get_supabase()
     response = supabase.table("Study_Material").select("*").execute()
     return response.data
 
-@router.post("/study_materials")
+@router.post("/study_material")
 def create_materials(study_material:StudyMaterialCreate):
     supabase = get_supabase()
     response = supabase.table("Study_Material").insert(study_material.model_dump()).execute()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
-from app.api import users
+from app.api import users, chat_session, message, progress, question, quiz, study_material, user_answer
 
 app = FastAPI(title="QuizCraft API")
 
+
+app.include_router(chat_session.router, prefix="/api", tags=["chat_sessions"])
+app.include_router(message.router, prefix="/api", tags=["messages"])
+app.include_router(progress.router, prefix="/api", tags=["progress"])
+app.include_router(question.router, prefix="/api", tags=["question"])
+app.include_router(quiz.router, prefix="/api", tags=["quiz"])
+app.include_router(study_material.router, prefix="/api", tags=["study_material"])
+app.include_router(user_answer.router, prefix="/api", tags=["answer"])
 app.include_router(users.router, prefix="/api", tags=["users"])
