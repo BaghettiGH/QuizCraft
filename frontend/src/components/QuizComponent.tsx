2709fbe55c5a0fe7e0aea2af8b4299a18c27b3fd
BaghettiGH <trollolol846@gmail.com>
2025-10-27 18:19:20 +0800

    add quizFix

--- frontend/src/components/QuizComponent.tsx
+++ frontend/src/components/QuizComponent.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef } from "react";
 import { Trophy, Loader2, Download, Eye, MoreVertical  } from "lucide-react";
 import { quizApi } from "@/services/api";
