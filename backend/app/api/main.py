50d60b06be55746c9ed14ab949871a14e3e7a37a
BaghettiGH <patrickmolina042@gmail.com>
2025-11-07 23:29:58 +0800

    removed mangum and uncommented imports

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-# from app.api import users, message, progress, question, quiz, study_material, user_answer, sessions, auth
-# from app.api import ai, explain
+from app.api import users, message, progress, question, quiz, study_material, user_answer, sessions, auth
+from app.api import ai, explain
 from fastapi.middleware.cors import CORSMiddleware
 # from fastapi.responses import FileResponse
 from fastapi.responses import Response
-from mangum import Mangum
+# from mangum import Mangum
 
 
 app = FastAPI(title="QuizCraft API")
@@ -50,22 +50,22 @@ async def debug_config():
 
 
 
-# app.include_router(sessions.router, prefix="/api", tags=["chat_sessions"])
-# app.include_router(message.router, prefix="/api", tags=["messages"])
-# app.include_router(progress.router, prefix="/api", tags=["progress"])
-# app.include_router(question.router, prefix="/api", tags=["question"])
-# app.include_router(study_material.router, prefix="/api", tags=["study_material"])
-# app.include_router(user_answer.router, prefix="/api", tags=["answer"])
-# app.include_router(users.router, prefix="/api", tags=["users"])
-# app.include_router(ai.router, prefix="/ai", tags=["AI"])
-# app.include_router(explain.router, prefix="/ai", tags=["AI Explain"])
-# app.include_router(sessions.router)
-# app.include_router(message.router)
-# app.include_router(quiz.router)
-# app.include_router(question.router)
-# app.include_router(user_answer.router)
-# app.include_router(auth.router)
+app.include_router(sessions.router, prefix="/api", tags=["chat_sessions"])
+app.include_router(message.router, prefix="/api", tags=["messages"])
+app.include_router(progress.router, prefix="/api", tags=["progress"])
+app.include_router(question.router, prefix="/api", tags=["question"])
+app.include_router(study_material.router, prefix="/api", tags=["study_material"])
+app.include_router(user_answer.router, prefix="/api", tags=["answer"])
+app.include_router(users.router, prefix="/api", tags=["users"])
+app.include_router(ai.router, prefix="/ai", tags=["AI"])
+app.include_router(explain.router, prefix="/ai", tags=["AI Explain"])
+app.include_router(sessions.router)
+app.include_router(message.router)
+app.include_router(quiz.router)
+app.include_router(question.router)
+app.include_router(user_answer.router)
+app.include_router(auth.router)
 
 
 
-handler = Mangum(app)
+# handler = Mangum(app)
